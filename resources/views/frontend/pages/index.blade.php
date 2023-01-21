7126ebbf96a9a2bc12dd691bf81c5599bc9a3f34
youssef-ahmed-Devlogs <youssefahmedsup@gmail.com>
2023-01-21 13:53:14 +0200

    Updates

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -7,7 +7,6 @@
     @endguest
     <!-- ====================== END LOGIN SECTION ====================== -->
 
-
     <!-- ====================== START hero section ====================== -->
 
     <section class="hero-section">
